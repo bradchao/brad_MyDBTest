ac2102a4eee48c02c07ca6f74a5846d37e9a3c5b
Brad <brad@brad.tw>
2016-10-13 19:56:07 +0800

    v5

--- app/src/main/java/brad/tw/mydbtest/MainActivity.java
+++ app/src/main/java/brad/tw/mydbtest/MainActivity.java
@@ -7,15 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MyOpenDBHelper helper;
     private SQLiteDatabase db;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv);
 
         helper = new MyOpenDBHelper(this,"brad",null,1);
         db = helper.getReadableDatabase();
@@ -32,17 +36,24 @@ public class MainActivity extends AppCompatActivity {
         query(null);
     }
 
+    public void delete(View v){
+        // DELETE FROM cust WHERE id = 3 and cname = 'brad'
+        db.delete("cust","id = ? and cname = ?",new String[]{"3","brad"});
+        query(null);
+    }
+
 
     public void query(View v){
         // SELECT * FROM cust
         //db.rawQuery()
+        tv.setText("");
         Cursor c = db.query("cust",null,null,null,null,null,null);
         while (c.moveToNext()){
-            String id = c.getString(0);
-            String cname = c.getString(1);
-            String tel = c.getString(2);
-            String birthday = c.getString(3);
-            Log.v("brad", id + ":" + cname + ":" + tel +":" + birthday);
+            String id = c.getString(c.getColumnIndex("id"));
+            String cname = c.getString(c.getColumnIndex("cname"));
+            String tel = c.getString(c.getColumnIndex("tel"));
+            String birthday = c.getString(c.getColumnIndex("birthday"));
+            tv.append(id + ":" + cname + ":" + tel +":" + birthday + "\n");
         }
     }
 
