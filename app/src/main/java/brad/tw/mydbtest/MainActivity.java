5f4b0d1a4db2bff35356bc8ee70ed5bd3c00beea
Brad <brad@brad.tw>
2016-10-13 19:25:16 +0800

    v3

--- app/src/main/java/brad/tw/mydbtest/MainActivity.java
+++ app/src/main/java/brad/tw/mydbtest/MainActivity.java
@@ -1,13 +1,37 @@
 package brad.tw.mydbtest;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private MyOpenDBHelper helper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helper = new MyOpenDBHelper(this,"brad",null,1);
+        db = helper.getReadableDatabase();
     }
+
+    public void query(View v){
+        // SELECT * FROM cust
+        //db.rawQuery()
+        Cursor c = db.query("cust",null,null,null,null,null,null);
+        while (c.moveToNext()){
+            String id = c.getString(0);
+            String cname = c.getString(1);
+            String tel = c.getString(2);
+            String birthday = c.getString(3);
+            Log.v("brad", id + ":" + cname + ":" + tel +":" + birthday);
+        }
+    }
+
+
 }
